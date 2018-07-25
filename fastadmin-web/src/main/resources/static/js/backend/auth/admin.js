76e09d0b0174dc52769db4e60a75a6085403c7dc
liwei <liwei@comtom.cn>
2018-07-25 17:05:07 +0800

    修改

--- fastadmin-web/src/main/resources/static/js/backend/auth/admin.js
+++ fastadmin-web/src/main/resources/static/js/backend/auth/admin.js
@@ -15,16 +15,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'vue', 'common'], fun
 
             var table = $("#table");
 
-            //在表格内容渲染完成后回调的事件
-            table.on('post-body.bs.table', function (e, json) {
-                $("tbody tr[data-index]", this).each(function () {
-                    //MY UPDATE || if (parseInt($("td:eq(1)", this).text()) == Config.admin.id) {
-                    if (parseInt($("td:eq(1)", this).text()) == 5) {
-                        $("input[type=checkbox]", this).prop("disabled", true);
-                    }
-                });
-            });
-
             // 初始化表格
             table.bootstrapTable({
                 //MY UPDATE |||| url: $.fn.bootstrapTable.defaults.extend.index_url,
@@ -40,13 +30,13 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'vue', 'common'], fun
                         {field: 'realname', title: "真实姓名", operate:false, formatter: Table.api.formatter.label},
                         {field: 'email', title: "邮箱"},
                         {field: 'status', title: "状态", formatter: function (value, row, index) {
-                                value = value == 1 ? "正常" : "禁用";
-                                return Table.api.formatter.status.call(this, value, row, index);
-                            }},
+                            value = value == 1 ? "正常" : "禁用";
+                            return Table.api.formatter.status.call(this, value, row, index);
+                        }},
                         {field: 'createTime', title: "创建时间",  sortable: true, formatter: function (value, row, index) {
-                                value = parseInt(value / 1000);
-                                return Table.api.formatter.datetime.call(this, value, row, index);
-                            }},
+                            value = parseInt(value / 1000);
+                            return Table.api.formatter.datetime.call(this, value, row, index);
+                        }},
                         {field: 'operate', title: "操作", table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
@@ -62,23 +52,30 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'vue', 'common'], fun
             var vm = new Vue({
                 el: "#main",
                 data: {
-                    row: {
-                        username: "hello"
-                    }
+                    row: {}
                 },
                 beforeCreate: function() {
-                    this.getDetail();
+                    var id = Common.urlParam("id")
+                    loadDetail(id);
                 },
                 mounted: function () {
                     Form.api.bindevent($("form[role=form]"));
                 },
                 methods: {
-                    getDetail: function () {
-                        var id = Common.urlParam("id");
-                        console.log(id);
-                    }
+
                 }
             });
+
+            function loadDetail(id) {
+                var url = "/sys/admin/detail/" + id
+                $.getJSON(url, {}, function (ret) {
+                    if (ret.ok) {
+                        vm.row = ret.data;
+                    } else {
+                        Toastr.error(ret.msg);
+                    }
+                });
+            }
 
         }
     };
